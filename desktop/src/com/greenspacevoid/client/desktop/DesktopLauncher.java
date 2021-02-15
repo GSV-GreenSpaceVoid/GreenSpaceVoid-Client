0f6b631d67440ee9e8f092959086b6f0fefc3199
GSV-GreenSpaceVoid <79030193+GSV-GreenSpaceVoid@users.noreply.github.com>
2021-02-14 21:45:26 -0600

    Added a few test methods and a listener

--- desktop/src/com/greenspacevoid/client/desktop/DesktopLauncher.java
+++ desktop/src/com/greenspacevoid/client/desktop/DesktopLauncher.java
@@ -2,7 +2,11 @@ package com.greenspacevoid.client.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.greenspacevoid.client.ui.menus.ui.main.TargetingMenu;
 import com.greenspacevoid.client.ui.renderer.GSVClient;
+
+import javax.swing.*;
+import java.lang.annotation.Target;
 
 public class DesktopLauncher {
 	public static final String version = "1.0.0 ALPHA";//Todo: Always be sure to change this before you build.
@@ -11,6 +15,12 @@ public class DesktopLauncher {
 
 
 	public static void main (String[] arg) {
+
+		//TargetingMenu.createTargetingMenu();
+
+		//TargetingMenu menu = new TargetingMenu();
+
+
 
 
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
